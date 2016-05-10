7033a8a844a1b8882c458d002b2be81b7792a767
nikthequik <quik.the.nick@gmail.com>
2016-05-10 11:17:57 -0600

    -change name attr of radio buttons to choice so only one can be selected at a time

--- js/app.js
+++ js/app.js
@@ -1,13 +1,3 @@
 $(document).ready(function(e) {
-   $(':radio').bind('change',function(){
-    var th = $(this), id = th.attr('id'); 
-    console.log(id);
-   if(th.is(':checked')){
-     $(':radio[id="'  + id + '"]').not($(this)).attr(':checked',false);   
-   }
-   else
-   {
-      $(':radio[id="'  + id + '"]').not($(this)).attr(':checked',true);  
-   }
-  });
+
 });
