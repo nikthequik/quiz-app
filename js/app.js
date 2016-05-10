bcbad7f1556961dd406e7149c1eadd7439c58b6e
nikthequik <quik.the.nick@gmail.com>
2016-05-10 10:51:48 -0600

    -add submit button -change choicebox position to absolute so it doesnt change when the paragraph does

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
-$(function() {
-/*	$('#tv-box').html('<p class="question">What was the first video game console called?</p>');
-*/});
+$(document).ready(function(e) {
+   $(':radio').bind('change',function(){
+    var th = $(this), id = th.attr('id'); 
+    console.log(id);
+   if(th.is(':checked')){
+     $(':radio[id="'  + id + '"]').not($(this)).attr(':checked',false);   
+   }
+   else
+   {
+      $(':radio[id="'  + id + '"]').not($(this)).attr(':checked',true);  
+   }
+  });
+});
