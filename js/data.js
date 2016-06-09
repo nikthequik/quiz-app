f31a96a2e3e001c6f8beb7df2278cba8a3832717
nikthequik <quik.the.nick@gmail.com>
2016-06-09 04:38:08 -0600

    -add data.js for question content

---
+++ js/data.js
@@ -0,0 +1,31 @@
+	var questions = [{
+		question: "In 2015, the New Horizons space probe sent us back the best pictures to date of the planet Pluto.  Which gaming console contained the same CPU as the space probe?",
+		choices: ['XBox', 'Playstation', 'Dreamcast', 'Atari'],
+		correct: 1,
+		qnum: 1,
+		explanation: 'The IBM processor used in the Playstation was known for its reliability, making it a perfect choice for powering a probe for long journey.'
+		},{
+		question: "What was the first video game console called?",
+		choices: ['Atari', 'Pong', 'Brown Box', 'NES'],
+		correct: 2,
+		qnum: 2,
+		explanation: 'The "Brown Box," though only a prototype, had basic features that most video games consoles still have today: two controls and a multigame program system.'
+		},{
+		question: "What is the best selling game console of all time?",
+		choices: ['Playstation 2', 'XBox 360', 'Nintendo 64', 'GameBoy'],
+		correct: 0,
+		qnum: 3,
+		explanation: 'When the Playstation 2 launched in 2000, there were long lines at every vendor.  To date they have sold over 157 million units.'
+		},{
+		question: "What was the first video game system to utilize DVD technology?",
+		choices: ['Dreamcast', 'Playstation', 'Playstation 2', 'XBox'],
+		correct: 2,
+		qnum: 2,
+		explanation: 'Gaming consoles began to expand their reach past their original purpose, and soon all gaming consoles came with the ability to play music and movies.'
+		},{
+		question: "What is the highest grossing arcade game of all time?",
+		choices: ['Pac-Man', 'Donkey Kong', 'Space Invaders', 'Street Fighter 2'],
+		correct: 0,
+		qnum: 5,
+		explanation: 'Pac-Man may be one of the most ubiquitous characters in video gaming history.  This game sold over 400,000 units and still acts as a symbol of retro gaming.'
+	}];
